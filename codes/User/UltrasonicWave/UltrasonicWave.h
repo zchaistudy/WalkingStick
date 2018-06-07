f186b6adb1e1c9569f2fbe6ba632c39554e082bc
zh <yzhiyzhi@126.com>
2018-06-08 01:17:45 +0800

    修改超声波部分

--- codes/User/UltrasonicWave/UltrasonicWave.h
+++ codes/User/UltrasonicWave/UltrasonicWave.h
@@ -4,33 +4,7 @@
 #include "stm32f10x.h"
 #include "bsp_delay.h"
 #include "bsp_usart1.h"
-#include "bsp_GeneralTim.h"
-
-
-
-
-
-
-#define	TRIG_PORT1     			  GPIOC		//TRIG       
-#define	TRIG_PIN1      			  GPIO_Pin_0   //TRIG       
-
-#define	TRIG_PORT2     			  GPIOC		//TRIG       
-#define	TRIG_PIN2      			  GPIO_Pin_1   //TRIG     
-
-#define	TRIG_PORT3     			  GPIOC		//TRIG       
-#define	TRIG_PIN3      			  GPIO_Pin_2   //TRIG       
-
-#define	TRIG_PORT4     			  GPIOC		//TRIG       
-#define	TRIG_PIN4      			  GPIO_Pin_3   //TRIG     
-
-#define	TRIG_PORT5     			  GPIOC		//TRIG       
-#define	TRIG_PIN5     			  GPIO_Pin_4   //TRIG 
-
-#define	TRIG_PORT6     			  GPIOC		//TRIG       
-#define	TRIG_PIN6      			  GPIO_Pin_5   //TRIG 
-
-
-#define RCC_APB2Periph_GPIOx  RCC_APB2Periph_GPIOC
+#include "waveConfig.h"
 
 #define ULTR_NUM        5              //超声波数量
 #define AVER_NUM        ULTR_NUM+1            //超声波数量+一位方位模块
@@ -41,7 +15,6 @@ int getRange(void);
 int minusDistance(int distance);
 int addDistance(int distance);
 void UltrasonicWave_Configuration(void);               //对超声波模块初始化
-void UltrasonicWave(int* num);
-void dealTIM_ICUserValueStructureData(TIM_ICUserValueTypeDef TIM_ICUserValueStructurex, int i);
+void UltrasonicWave(int);
 #endif /* __UltrasonicWave_H */
 
